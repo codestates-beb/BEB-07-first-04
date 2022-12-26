7536faecf90d79bb26be17119fe65ddd6acd0e55
oreozzang <93peace@naver.com>
2022-12-26 15:32:28 +0900

    cache clear

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -9,9 +9,14 @@ const Slider = () => {
         <div className="main-slider-heading-wrapper">
             <h1>Explore, collect and sell NFTs</h1>
         </div>
-        <button className="main-slider-left-arrow"></button>
-        <button className="main-slider-right-arrow"></button>
+        <button className="main-slider-arrow-button main-slider-left-arrow">
+            <svg xmlns="http://www.w3.org/2000/svg" id="Bold" viewBox="0 0 24 24"><path d="M10.957,12.354a.5.5,0,0,1,0-.708l4.586-4.585a1.5,1.5,0,0,0-2.121-2.122L8.836,9.525a3.505,3.505,0,0,0,0,4.95l4.586,4.586a1.5,1.5,0,0,0,2.121-2.122Z"/></svg>
+        </button>
+        <button className="main-slider-arrow-button main-slider-right-arrow">
+           <svg xmlns="http://www.w3.org/2000/svg" id="Bold" viewBox="0 0 24 24" ><path d="M15.75,9.525,11.164,4.939A1.5,1.5,0,0,0,9.043,7.061l4.586,4.585a.5.5,0,0,1,0,.708L9.043,16.939a1.5,1.5,0,0,0,2.121,2.122l4.586-4.586A3.505,3.505,0,0,0,15.75,9.525Z"/></svg>
+        </button>
         <div className="main-swiper-container">
+            <Swiper></Swiper>
             <Swiper></Swiper>
             <Swiper></Swiper>
             <Swiper></Swiper>
--- client/src/components/Swiper.js
+++ client/src/components/Swiper.js
@@ -4,10 +4,13 @@ import "./Swiper.css";
 const Swiper = () => {
 
     return <div className="main-slider-swiper">
-        <div className="main-slider-swiper-description">
-            <h5>NFT 제목</h5>
-            <span>Floor : 0.112ETH</span>
-        </div>
+            <span className="">
+                <img className="main-slider-swiper-bgimg" src="https://i.seadn.io/gcs/files/1f3dae543849e6769de98fdb49780398.png?auto=format&w=1000" alt="image"></img>
+            </span>
+            <div className="main-slider-swiper-description">
+                <h5>NFT 제목</h5>
+                <span>Floor : 0.112ETH</span>
+            </div>
     </div>  
 }
 
