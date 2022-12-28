a0f0190f564a57632fe4f79f8230333695347ed0
hgunh16 <hgunh16@gmail.com>
2022-12-29 00:15:55 +0900

    header, footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,45 @@
 import React from "react";
-
+import { AiFillGithub,AiFillTwitterCircle,AiFillFacebook,AiFillInstagram,AiFillYoutube } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import './Footer.css'
 const Footer = () =>{
     return (
-      <div className="footer">©OPEN C</div>      
+      
+      <div className="footerMain">
+      <Link to="/">    
+        <div className="logoContainer">
+          <div className="logoText">OpenC</div>
+          <input className="emailBar"placeholder="your email address"></input>
+          <button>Subscribe</button>
+        </div>
+      </Link>
+
+      <div className="footerItems">
+        <div className="githubIcon">
+          <AiFillGithub/>
+        </div>
+      
+        <div className="twitterIcon">
+          <AiFillTwitterCircle />
+        </div>
+
+        <div className="facebookIcon">
+          <AiFillFacebook />
+        </div>
+        <div className="instagramIcon">
+          <AiFillInstagram />
+        </div>
+      
+        <div className="youtubeIcon">
+          <AiFillYoutube />
+        </div>
+      </div>
+      
+      <div className="footerUnder">
+        <p>© 2022 BEB,07, Inc</p>
+      </div>
+    
+    </div>    
     )
   }
 
